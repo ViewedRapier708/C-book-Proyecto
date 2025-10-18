f610a9e4e4cdc04ba32031ddb6eb8cbc5ca77191
ViewedRapier708 <jdelenac2300@alumno.ipn.mx>
2025-10-18 17:24:18 -0600

    Js de validacion de campos

--- funciones/funcionesIndex/validacionCampos.js
+++ funciones/funcionesIndex/validacionCampos.js
@@ -1,3 +1,14 @@
-function validacionCampos(campos) {
-    
+function ValidarCamposNumericos(e) {
+    var teclado =(document.all)? e.keyCode : e.which
+    if (teclado== 8 ) return true;
+    var patron =/[0-9]/;
+    var codigo=String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+function ValidarCamposPassword(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true; // Backspace
+    var patron = /[A-Za-z\d@$!%*?&]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
 }
