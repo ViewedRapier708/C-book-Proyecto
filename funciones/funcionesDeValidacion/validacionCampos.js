657d994faf1543efa4ef06a15d94def0897f0d24
ViewedRapier708 <jdelenac2300@alumno.ipn.mx>
2025-10-21 00:29:08 -0600

    funciones validacionCampos

--- funciones/funcionesDeValidacion/validacionCampos.js
+++ funciones/funcionesDeValidacion/validacionCampos.js
@@ -1,3 +1,5 @@
+
+//Funciones para validar los campos de los formularios al momento de escribir
 function ValidarCamposNumericos(e) {
     var teclado =(document.all)? e.keyCode : e.which
     if (teclado== 8 ) return true;
@@ -12,11 +14,21 @@ function ValidarCamposPassword(e) {
     var codigo = String.fromCharCode(teclado);
     return patron.test(codigo);
 }
-
 function ValidarCamposLetras(e) {
     var teclado = (document.all) ? e.keyCode : e.which;
     if (teclado == 8) return true; // Backspace
     var patron = /[A-Za-z\s\'\á\é\í\ó\ú]/;
     var codigo = String.fromCharCode(teclado);
     return patron.test(codigo);
+}
+
+//Funcion para enviar el formulario despues de validar los campos
+function EnviarFormulario() {
+    if (ValidarCamposVacios()) {
+        // Si los campos están vacíos, no se envía el formulario
+        alert("Por favor, complete todos los campos.");
+        return false;
+    }
+    // Si todos los campos son válidos, se puede enviar el formulario
+    document.getElementById("form-registro").submit();
 }
