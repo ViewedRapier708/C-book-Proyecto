af19fe8d71f0b4e44ee1838a97d01a9321fe87a0
ViewedRapier708 <jdelenac2300@alumno.ipn.mx>
2025-11-21 11:00:52 -0600

    icono de cargando e inicio de la funcion para la verificacion

--- pantallasUs/js/registro.js
+++ pantallasUs/js/registro.js
@@ -1,18 +1,43 @@
 function registro(event){
         event.preventDefault();
     //Se recuperan los datos del formulario
+    const regularExpCorreo=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const regularExpPassword=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,16}$/;
     const boleta=document.getElementById("boleta").value;
     const correo=document.getElementById("correo").value;
     const password=document.getElementById("password").value;
     const confPsw=document.getElementById("confPsw").value;
     const mensajeDiv=document.querySelector('.messaje');
+
+    //Validaciones para el registro
     if(boleta==="" || correo==="" || password===""){
        mensajeDiv.textContent='Por favor, complete todos los campos';
        mensajeDiv.style.color='red';
        return;
+    }else if(boleta.length < 10){
+        mensajeDiv.textContent='La boleta debe tener al menos 10 caracteres';
+        mensajeDiv.style.color='red';
+        return;
+    }else if(!regularExpCorreo.test(correo)){
+        mensajeDiv.textContent='El formato del correo es incorrecto';
+        mensajeDiv.style.color='red';
+        return;
+    }else if(!regularExpPassword.test(password)){
+        mensajeDiv.textContent='La contraseña debe tener entre 6 y 16 caracteres, incluir al menos una letra mayúscula, una letra minúscula, un número y un carácter especial';
+        mensajeDiv.style.color='red';
+        return;
     }
-}
-function verificarConfirmacion(){
-    //En esta funcion se verificara si el usuario ya confirmo su correo, esto se hara mediante un bucle que cheque cada 3 segundos si el usuario ya confirmo su correo
+    else if(password.length < 6 || password.length > 16){
+        mensajeDiv.textContent='El formato del correo es incorrecto';
+        mensajeDiv.style.color='red';
+        return;
+    }
+    else if(password!==confPsw){
+        mensajeDiv.textContent='Las contraseñas no coinciden';
+        mensajeDiv.style.color='red';
+        return;
+    }
 
+    //Petición al servidor
+    
 }
