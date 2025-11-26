70478e11403149cb71e226307e8edf74cfd71225
EnriqueTablerosGarcia <etablerosg2300@alumno.ipn.mx>
2025-11-26 16:38:38 -0600

    hola jeje

--- back/app.js
+++ back/app.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const authRoutes = require('./src/routes/Rutas.js'); // ajusta la ruta según tu proyecto
+const adminRoutes = require('./src/routes/RutasAdmin.js'); // Rutas de administrador
 require('dotenv').config();
 
 // Middleware para leer JSON
@@ -18,6 +19,9 @@ app.get('/', (req, res) => {
 
 // Rutas de autenticación
 app.use('/auth', authRoutes);
+
+// Rutas de administrador
+app.use('/api/admin', adminRoutes);
 
 // Puerto
 const PORT = process.env.PORT || 3000;
