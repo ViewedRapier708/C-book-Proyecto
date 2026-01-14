eb1ed0490418bc0611bd48fdd69297ff7109e42b
ViewedRapier708 <jdelenac2300@alumno.ipn.mx>
2026-01-14 15:23:12 -0600

    avance de la funcion

--- back/jobs/VerificacionAsistencia.js
+++ back/jobs/VerificacionAsistencia.js
@@ -8,6 +8,8 @@ async function verificarAsistencia(params) {
     Act_restiradores=actividades.Restiradores
     Solicicitudes_libros=actividades.Libros
 
+
+    
     console.log("Verificación de asistencia ejecutada");
 //debug
     console.log("Actividades de computadoras:", Act_computadoras);
@@ -19,9 +21,9 @@ async function verificarAsistencia(params) {
 const obtenerActividades = async () => {
 const { getClient } = require('../config/db');
 const supabase = getClient();
-const Computadoras = await supabase.from('solicitudes_computadora').select('*').eq();
-const Restiradores = await supabase.from('solicitudes_restirador').select('*');
-const Libros = await supabase.from('solicitudes_libros').select('*');
+const Computadoras = await supabase.from('solicitudes_computadora').select('*').eq("estado_asistencia_id",1);
+const Restiradores = await supabase.from('solicitudes_restirador').select('*').eq("estado_asistencia_id",1);
+const Libros = await supabase.from('solicitudes_libros').select('*').eq("estado_solicitud_id",1);
 
 return {Computadoras,Restiradores,Libros};
     // Lógica para obtener las actividades programadas desde la base de datos
